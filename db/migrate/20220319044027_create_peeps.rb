9a9c3bbe6c73557707a247c5bdec3804707c9bec
Pat <Patgiok@gmail.com>
2022-03-19 21:40:38 -0700

    added models, basic funcs, seed

--- db/migrate/20220319044027_create_peeps.rb
+++ db/migrate/20220319044027_create_peeps.rb
@@ -1,6 +1,6 @@
 class CreatePeeps < ActiveRecord::Migration[7.0]
   def change
-    create_table :peeps do |t|
+    create_table :peep do |t|
       t.string :firstname
       t.string :lastname
       t.string :email
--- db/migrate/20220319044127_create_cars.rb
+++ db/migrate/20220319044127_create_cars.rb
@@ -5,7 +5,7 @@ class CreateCars < ActiveRecord::Migration[7.0]
       t.string :make
       t.string :model
       t.integer :price
-      t.belongs_to :Peep, null: false, foreign_key: true
+      t.belongs_to :peep, null: false, foreign_key: true
 
       t.timestamps
     end
